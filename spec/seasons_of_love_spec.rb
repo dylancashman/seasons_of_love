ead6055b383e472074d6de630af8dc118c1b315f
Dylan Cashman <dylan.cashman@annkissam.com>
2013-11-12 12:57:10 -0500

    refactored specs

--- spec/seasons_of_love_spec.rb
+++ spec/seasons_of_love_spec.rb
@@ -1,27 +1,31 @@
 require File.join(File.dirname(__FILE__), '..', 'lib', 'seasons_of_love')
 
 describe SeasonsOfLove do
-  let(:parsed_range) { SeasonsOfLove.split_dates_into_ranges(@start_date, @end_date) }
-
-  context "parse_range_into_months_hash" do
+  
+  context "parse range into months hash" do
     context "same month" do
+      let(:start_date) { Date.parse("September 5, 2012") }
+      let(:end_date) { Date.parse("September 18, 2012") }
+      let(:parsed_range) { SeasonsOfLove.split_dates_into_ranges(start_date, end_date) }
+
       it 'should return one hash' do
-        @start_date = Date.parse("September 5, 2012")
-        @end_date = Date.parse("September 18, 2012")
-        parsed_range.should == [{:start_date => @start_date, :end_date => @end_date, :month => "September"}]
+        parsed_range.should == [{:start_date => start_date, :end_date => end_date, :month => "September"}]
       end
     end
 
     context "different months" do
+      let(:start_date) { Date.parse("September 5, 2012") }
+      let(:end_date) { Date.parse("November 18, 2012") }
+      let(:parsed_range) { SeasonsOfLove.split_dates_into_ranges(start_date, end_date) }
+
       it 'should return multiple hashes' do
-        @start_date = Date.parse("September 5, 2012")
-        @end_date = Date.parse("November 18, 2012")
         parsed_range.should ==  [
-                                  {:start_date => @start_date, :end_date => Date.parse("September 30, 2012"), :month => "September"},
+                                  {:start_date => start_date, :end_date => Date.parse("September 30, 2012"), :month => "September"},
                                   {:start_date => Date.parse("October 1, 2012"), :end_date => Date.parse("October 31, 2012"), :month => "October"},
                                   {:start_date => Date.parse("November 1, 2012"), :end_date => Date.parse("November 18, 2012"), :month => "November"}
                                 ]
       end
     end
   end
+
 end
